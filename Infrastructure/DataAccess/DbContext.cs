e1fa501855ee1776315b5de626c4dc5707832d65
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-07-23 13:32:51 +0800

    🌐 infrastructure: service container

---
+++ Infrastructure/DataAccess/DbContext.cs
@@ -0,0 +1,11 @@
+﻿
+using Application.Extension.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DataAccess
+{
+    public class DbContext (DbContextOptions<DbContext> options): IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
---
+++ Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,44 @@
+﻿using Application.Extension.Identity;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using DbContext = Infrastructure.DataAccess.DbContext;
+
+
+namespace Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DbContext>(opt => opt.UseSqlServer(config.GetConnectionString("Default")), ServiceLifetime.Scoped);
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultScheme = IdentityConstants.ApplicationScheme;
+                opt.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            }).AddIdentityCookies();
+
+            services.AddIdentityCore<ApplicationUser>()
+                .AddEntityFrameworkStores<DbContext>()
+                .AddSignInManager()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthorizationBuilder()
+                 .AddPolicy("AdministrationPolicy", adp =>
+                 {
+                     adp.RequireAuthenticatedUser();
+                     adp.RequireRole("Admin", "ManagerRole");
+                 })
+                 .AddPolicy("UserPolicy", adp =>
+                 {
+                     adp.RequireAuthenticatedUser();
+                     adp.RequireRole("User");
+                 });
+
+            return services;
+        }
+    }
+}
