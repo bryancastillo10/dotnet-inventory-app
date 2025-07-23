51f37d0383112fc6a1d8142548fe207fe4233d4f
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-07-23 10:22:10 +0800

    📂 Request DTO

---
+++ Application/DTO/Request/Identity/LoginUserRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTO.Request.Identity
+{
+     public class LoginUserRequestDTO
+    {
+        [EmailAddress]
+        [RegularExpression("[@^\\t\\r\\n]+@[^@ \\t\\r\\n]+\\.[^@ \\t\\r\n]+", ErrorMessage = "Invalid email format")]
+
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$ %^&*-]).{8,}$", ErrorMessage="Your password is incorrect")]
+        [MinLength(8), MaxLength(100)]
+
+        public string Password { get; set; }
+    }
+}
